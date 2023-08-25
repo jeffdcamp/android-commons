c1f5f607ff625dd8273f7c2f9ab2001815303658
jeffdcamp <jeffdcamp@gmail.com>
2023-08-25 14:51:11 -0600

    Improved SavedStateHandleExt.kt: Removed generic get() to prevent runtime issues / created SavedStateHandle extensions that are type safe (return a value that is expected)

--- library/src/main/java/org/dbtools/android/commons/ext/SavedStateHandleExt.kt
+++ library/src/main/java/org/dbtools/android/commons/ext/SavedStateHandleExt.kt
@@ -2,10 +2,16 @@ package org.dbtools.android.commons.ext
 
 import androidx.lifecycle.SavedStateHandle
 
-/**
- * Use when you wish to receive a non-null default value from the [SavedStateHandle]
- * when a saved state value does not yet exist for the given key.
- */
-inline fun <reified T> SavedStateHandle.get(key: String, default: T): T {
-    return get(key) ?: default
-}
+fun createSaveStateErrorMessage(key: String) = "Missing SavedState value for Key: $key"
+
+fun SavedStateHandle.requireString(key: String): String = requireNotNull(get<String>(key)) { createSaveStateErrorMessage(key) }
+fun SavedStateHandle.requireInt(key: String): Int = requireNotNull(get<Int>(key)) { createSaveStateErrorMessage(key) }
+fun SavedStateHandle.requireLong(key: String): Long = requireNotNull(get<Long>(key)) { createSaveStateErrorMessage(key) }
+fun SavedStateHandle.requireFloat(key: String): Float = requireNotNull(get<Float>(key)) { createSaveStateErrorMessage(key) }
+fun SavedStateHandle.requireBoolean(key: String): Boolean = requireNotNull(get<Boolean>(key)) { createSaveStateErrorMessage(key) }
+
+fun SavedStateHandle.getString(key: String): String? = get<String>(key)
+fun SavedStateHandle.getInt(key: String): Int? = get<Int>(key)
+fun SavedStateHandle.getLong(key: String): Long? = get<Long>(key)
+fun SavedStateHandle.getFloat(key: String): Float? = get<Float>(key)
+fun SavedStateHandle.getBoolean(key: String): Boolean? = get<Boolean>(key)
