2766aff0338b406c6d52a665581612481ded0ceb
jeffdcamp <jeffdcamp@gmail.com>
2023-10-07 08:44:39 -0600

    Removed ThreeTenExt (Date.toOffsetDateTime())

--- library/src/main/java/org/dbtools/android/commons/ext/ThreeTenExt.kt
+++
@@ -1,7 +0,0 @@
-package org.dbtools.android.commons.ext
-
-import java.time.OffsetDateTime
-import java.time.ZoneOffset
-import java.util.Date
-
-fun Date.toOffsetDateTime(): OffsetDateTime = toInstant().atOffset(ZoneOffset.UTC)
