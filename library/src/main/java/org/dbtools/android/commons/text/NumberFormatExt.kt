a972ab85217beea4d653e9600576eb9cd4acba00
jeffdcamp <jeffdcamp@gmail.com>
2024-01-30 17:03:08 -0700

    Added NumberFormatExt: isNumeric(), trimTrailingZero(), toStringTrimTrailingZero(), toRoundedText(), toPercentageText()

---
+++ library/src/main/java/org/dbtools/android/commons/text/NumberFormatExt.kt
@@ -0,0 +1,49 @@
+@file:Suppress("ImplicitDefaultLocale")
+package org.dbtools.android.commons.text
+
+fun Double.toPercentageText(): String {
+    return "${String.format("%.0f", this * 100)}%"
+}
+
+fun Double.toRoundedText(decimalPlaces: Int = 1, trimTrailingZero: Boolean = true): String {
+    val text = String.format("%.${decimalPlaces}f", this)
+    return if (trimTrailingZero) {
+        text.trimTrailingZero()
+    } else {
+        text
+    }
+}
+
+fun String.trimTrailingZero(): String {
+    return if (isNotBlank()) {
+        if (indexOf(".") < 0) {
+            this
+        } else {
+            replace("0*$".toRegex(), "").replace("\\.$".toRegex(), "")
+        }
+    } else {
+        this
+    }
+}
+
+fun Double.toStringTrimTrailingZero(): String {
+    return toString().trimTrailingZero()
+}
+
+fun String.isNumeric(allowHangingDecimal: Boolean = false): Boolean {
+    if (allowHangingDecimal && equals(".")) {
+        return true
+    }
+
+    if (allowHangingDecimal && endsWith(".")) {
+        val beforeLastDecimal = take(this.length - 1)
+        return beforeLastDecimal.isNumeric()
+    }
+
+    if (startsWith(".")) {
+        return takeLast(this.length - 1).isNumeric()
+    }
+
+    val regex = "-?[0-9]+(\\.[0-9]+)?".toRegex()
+    return matches(regex)
+}
