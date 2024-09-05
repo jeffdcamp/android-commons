8498ac685c22f043cb174d9564ee863fd3bed4bc
jeffdcamp <jeffdcamp@gmail.com>
2024-09-05 05:29:25 -0600

    Added Instant.dayOfWeek(...), Instant.nextDayOfWeek(...), Instant.previousOrSameDayOfWeek(...)

--- library/src/main/java/org/dbtools/android/commons/ext/KotlinDateTimeExt.kt
+++ library/src/main/java/org/dbtools/android/commons/ext/KotlinDateTimeExt.kt
@@ -1,8 +1,35 @@
 package org.dbtools.android.commons.ext
 
+import kotlinx.datetime.DayOfWeek
 import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.isoDayNumber
+import kotlinx.datetime.toLocalDateTime
+import kotlin.time.Duration.Companion.days
 import kotlin.time.Duration.Companion.nanoseconds
 
 fun Instant.trimToSeconds(): Instant {
     return minus(nanosecondsOfSecond.nanoseconds)
+}
+
+fun Instant.dayOfWeek(timeZone: TimeZone = TimeZone.currentSystemDefault()): DayOfWeek = toLocalDateTime(timeZone).dayOfWeek
+
+fun Instant.nextDayOfWeek(
+    dayOfWeek: DayOfWeek,
+    timeZone: TimeZone = TimeZone.currentSystemDefault(),
+): Instant {
+    val daysDiff = dayOfWeek(timeZone).isoDayNumber - dayOfWeek.isoDayNumber
+    val daysToAdd = if (daysDiff >= 0) 7 - daysDiff else -daysDiff
+    return plus(daysToAdd.days)
+}
+
+fun Instant.previousOrSameDayOfWeek(
+    dayOfWeek: DayOfWeek,
+    timeZone: TimeZone = TimeZone.currentSystemDefault(),
+): Instant {
+    val daysDiff = dayOfWeek.isoDayNumber - dayOfWeek(timeZone).isoDayNumber
+    if (daysDiff == 0) return this
+
+    val daysToAdd = if (daysDiff >= 0) 7 - daysDiff else -daysDiff
+    return minus(daysToAdd.days)
 }
